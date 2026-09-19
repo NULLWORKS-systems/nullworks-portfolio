0e43b3aaa87204bc8cbaa825eab32356b1a1de6f
Mason Perry <masoncalcolsol@gmail.com>
2026-09-19 06:40:20 -0700

    Add Smart as Shit media assemblers

---
+++ src/app/smart-as-shit/_media/pdf.ts
@@ -0,0 +1,9 @@
+import { pdf0 } from "./pdf0";
+import { pdf1 } from "./pdf1";
+import { pdf2 } from "./pdf2";
+import { pdf3 } from "./pdf3";
+import { pdf4 } from "./pdf4";
+import { pdf5 } from "./pdf5";
+import { pdf6 } from "./pdf6";
+
+export const pdf = pdf0 + pdf1 + pdf2 + pdf3 + pdf4 + pdf5 + pdf6;
---
+++ src/app/smart-as-shit/_media/png.ts
@@ -0,0 +1,7 @@
+import { png0 } from "./png0";
+import { png1 } from "./png1";
+import { png2 } from "./png2";
+import { png3 } from "./png3";
+import { png4 } from "./png4";
+
+export const png = png0 + png1 + png2 + png3 + png4;
